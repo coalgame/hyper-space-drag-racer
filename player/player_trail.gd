b47318dd37778344af0493d658baf434141495e3
Will <43840962+willn1337@users.noreply.github.com>
2026-05-14 13:58:37 +1000

    k

--- player/player_trail.gd
+++ player/player_trail.gd
@@ -49,5 +49,5 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
 		if body.is_multiplayer_authority():
-			body.speed_boost(0.2)
+			body.speed_boost(0.4)
 			queue_free()
